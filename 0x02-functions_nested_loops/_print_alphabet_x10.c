d35281dfcc459f88b56a0b98b949a303eaef7e58
Ayshataje <imagbyai@gmail.com>
2023-04-25 18:47:02 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/_print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+
+{
+	char c;
+	int i =0;
+
+	while (i <== 9)
+	{
+	_purtchar(c);
+
+	_purtchar('\n');
+	i++;
+	}
