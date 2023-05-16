1e9806891f77d076a8ab526485eb7603fd6945bd
Ayshataje <imagbyai@gmail.com>
2023-05-16 10:45:40 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,24 @@
-#include<stddef.h>
-#include<stdlib.h>
+#ifndef MAIN_H
+		
+#define MAIN_H
+		
+
+		
 char *create_array(unsigned int size, char c);
+		
+int _putchar(char c);
+		
 char *_strdup(char *str);
-int _strlen(char *s);
+		
 char *str_concat(char *s1, char *s2);
+		
 int **alloc_grid(int width, int height);
+		
 void free_grid(int **grid, int height);
+		
+char *argstostr(int ac, char **av);
+		
+
+		
+#endif
+
